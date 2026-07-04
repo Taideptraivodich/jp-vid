82b2feda3c3848b9b51f00ab1625b77639e5c6ce
Taideptraivodich <nguyenkhanhtai30@gmail.com>
2026-07-04 14:58:14 +0700

    Update server.js

--- course-video-player/server.js
+++ course-video-player/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 const PORT = Number(process.env.PORT || 8080);
-const API_ROOT = process.env.API_ROOT || 'https://api-online.abc.vn/api/v1/client';
+const API_ROOT = process.env.API_ROOT || 'https://api-online.riki.edu.vn/api/v1/client';
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
